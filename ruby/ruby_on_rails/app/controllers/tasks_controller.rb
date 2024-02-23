90df2625c5623e333d69993da3a4a4530d28aa95
Louis Ramos <louisramosdev@gmail.com>
2024-02-23 18:28:19 +0100

    ruby_on_rails: Create TasksController

---
+++ ruby/ruby_on_rails/app/controllers/tasks_controller.rb
@@ -0,0 +1,38 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: %i[complete destroy]
+
+  def index
+    render json: { tasks: Task.all }, status: :ok
+  end
+
+  def create
+    task = Task.new(task_params)
+
+    if task.save
+      render json: { task: task }, status: :created
+    else
+      render json: { errors: task.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task.destroy
+
+    render status: :ok
+  end
+
+  def complete
+    @task.update(completed: true)
+    render json: { task: @task }, status: :ok
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title)
+  end
+end
---
+++ ruby/ruby_on_rails/spec/controller/tasks_controller_spec.rb
@@ -0,0 +1,115 @@
+require "rails_helper"
+
+RSpec.describe TasksController, type: :request do
+  describe "GET /tasks" do
+    before do
+      create(:task)
+      get "/tasks"
+    end
+
+    it "returns a JSON object" do
+      expect(response.body).to be_a String
+      expect(response.parsed_body).to have_key "tasks"
+    end
+
+    it "returns a list of Task" do
+      data = response.parsed_body
+      expect(data["tasks"]).to contain_exactly hash_including("title" => "My task", "completed" => false)
+    end
+
+    it "returns a HTTP status of success" do
+      expect(response).to have_http_status :success
+    end
+  end
+
+  describe "POST /tasks" do
+    context "with proper param" do
+      before do
+        post "/tasks", params: { task: { title: "My task" } }
+      end
+
+      it "returns a JSON object" do
+        expect(response.body).to be_a String
+        expect(response.parsed_body).to have_key "task"
+      end
+
+      it "creates an instance of Task" do
+        expect(Task.count).to eq 1
+      end
+
+      it "returns the new instance of Task" do
+        data = response.parsed_body
+        expect(data.dig("task", "title")).to eq "My task"
+      end
+
+      it "returns a HTTP status of created" do
+        expect(response).to have_http_status :created
+      end
+    end
+
+    context "without proper params" do
+      before do
+        post "/tasks", params: { task: { title: nil } }
+      end
+
+      it "returns a JSON object" do
+        expect(response.body).to be_a String
+        expect(response.parsed_body).to have_key "errors"
+      end
+
+      it "does not create an instance of Task" do
+        expect(Task.count).to eq 0
+      end
+
+      it "returns a list of error messages" do
+        data = response.parsed_body
+        expect(data["errors"]).to contain_exactly "Title can't be blank"
+      end
+
+      it "returns a HTTP status of unprocessable_entity" do
+        expect(response).to have_http_status :unprocessable_entity
+      end
+    end
+  end
+
+  describe "DELETE /tasks/:id" do
+    before do
+      task = create(:task)
+      delete "/tasks/#{task.id}"
+    end
+
+    it "destroys the instance of Task" do
+      expect(Task.count).to be 0
+    end
+
+    it "returns a HTTP status of success" do
+      expect(response).to have_http_status :success
+    end
+  end
+
+  describe "GET /tasks/:id/complete" do
+    let!(:task) { create(:task) }
+
+    before do
+      patch "/tasks/#{task.id}/complete"
+    end
+
+    it "returns a JSON object" do
+      expect(response.body).to be_a String
+      expect(response.parsed_body).to have_key "task"
+    end
+
+    it "returns the updated instance of Task" do
+      data = response.parsed_body
+      expect(data.dig("task", "id")).to be task.id
+    end
+
+    it "marks the Task instance as completed" do
+      expect(Task.find(task.id)).to be_completed
+    end
+
+    it "returns a HTTP status of success" do
+      expect(response).to have_http_status :success
+    end
+  end
+end
