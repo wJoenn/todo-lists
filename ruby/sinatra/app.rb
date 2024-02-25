1d687a247d2551a51e73b1100e9ffa8d5677e4e4
Louis Ramos <louisramosdev@gmail.com>
2024-02-25 18:07:38 +0100

    sinatra: create tasks routes

--- ruby/sinatra/app.rb
+++ ruby/sinatra/app.rb
@@ -5,3 +5,29 @@ get "/tasks" do
   status 200
   Task.all.to_json
 end
+
+post "/tasks" do
+  body = JSON.parse(request.body.read).deep_transform_keys(&:to_sym)
+  task_params = body[:task].slice(:title)
+  task = Task.new(task_params)
+
+  if task.save
+    status 200
+    task.to_json
+  else
+    status 404
+    { errors: task.errors.full_messages }.to_json
+  end
+end
+
+delete "/tasks/:id" do
+  task = Task.find(params["id"])
+  task.destroy
+  status 200
+end
+
+patch "/tasks/:id/complete" do
+  task = Task.find(params["id"])
+  task.update(completed: true)
+  status 200
+end
