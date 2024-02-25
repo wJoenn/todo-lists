6f9792521e64ed7e5caaefca49a74d8630189807
Louis Ramos <75388869+wJoenn@users.noreply.github.com>
2024-02-25 17:48:46 +0100

    ci: fix sinatra RSpec github action (#2)

--- ruby/sinatra/config/application.rb
+++ ruby/sinatra/config/application.rb
@@ -1,7 +1,7 @@
 require "bundler"
 Bundler.require(:default)
 
-ENV["RAILS_ENV"] ||= "development"
+ENV["RACK_ENV"] ||= "development"
 
 Dir["#{__dir__}/initializers/*.rb"].each { |file| require file }
 Dir["#{__dir__}/../models/**/*.rb"].each { |file| require file }
