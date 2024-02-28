ae091618b63b2092c63e8e1b2f273799f4b468ef
Louis Ramos <louisramosdev@gmail.com>
2024-02-28 20:06:24 +0100

    sinatra: moved models and services in app directory

--- ruby/sinatra/config/application.rb
+++ ruby/sinatra/config/application.rb
@@ -4,7 +4,6 @@ Bundler.require(:default)
 ENV["RAILS_ENV"] ||= "development"
 
 Dir["#{__dir__}/initializers/*.rb"].each { |file| require file }
-Dir["#{__dir__}/../models/**/*.rb"].each { |file| require file }
-Dir["#{__dir__}/../services/**/*.rb"].each { |file| require file }
+Dir["#{__dir__}/../app/**/*.rb"].each { |file| require file }
 
 set :port, 3000
