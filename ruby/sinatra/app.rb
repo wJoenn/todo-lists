7283f5f5438415ba4b7173e4e8cb84547b369838
Louis Ramos <louisramosdev@gmail.com>
2024-02-29 21:43:30 +0100

    sinatra: reorder routes in app.rb

--- ruby/sinatra/app.rb
+++ ruby/sinatra/app.rb
@@ -25,43 +25,6 @@ get "/current_user" do
   @current_user.to_json
 end
 
-post "/users" do
-  user_params = parsed_params(:user, %i[email password password_confirmation])
-  user = User.new(user_params)
-
-  if user.save
-    status 202
-    response.headers["Authorization"] = user.jwt
-    user.to_json
-  else
-    status 422
-    { errors: user.errors.full_messages }.to_json
-  end
-end
-
-post "/users/sign_in" do
-  user_params = parsed_params(:user, %i[email password])
-  user = User.find_by(email: user_params[:email])
-
-  if user.present? && user.password == user_params[:password]
-    status 200
-    response.headers["Authorization"] = user.jwt
-    user.to_json
-  else
-    status 401
-    { errors: ["Invalid Email or Password"] }.to_json
-  end
-end
-
-delete "/users/sign_out" do
-  authenticate_user!
-
-  @current_user.edit_jti
-  @current_user.save
-
-  status 200
-end
-
 get "/tasks" do
   authenticate_user!
 
@@ -101,6 +64,43 @@ patch "/tasks/:id/complete" do
 
   status 200
   task.to_json
+end
+
+post "/users" do
+  user_params = parsed_params(:user, %i[email password password_confirmation])
+  user = User.new(user_params)
+
+  if user.save
+    status 202
+    response.headers["Authorization"] = user.jwt
+    user.to_json
+  else
+    status 422
+    { errors: user.errors.full_messages }.to_json
+  end
+end
+
+post "/users/sign_in" do
+  user_params = parsed_params(:user, %i[email password])
+  user = User.find_by(email: user_params[:email])
+
+  if user.present? && user.password == user_params[:password]
+    status 200
+    response.headers["Authorization"] = user.jwt
+    user.to_json
+  else
+    status 401
+    { errors: ["Invalid Email or Password"] }.to_json
+  end
+end
+
+delete "/users/sign_out" do
+  authenticate_user!
+
+  @current_user.edit_jti
+  @current_user.save
+
+  status 200
 end
 
 options "*" do
