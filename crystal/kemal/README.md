cd40b23e579c61269184eb0916f3dc6cdb8bc457
Louis Ramos <louisramosdev@gmail.com>
2024-03-02 23:47:55 +0100

    kemal: add tasks routes

--- crystal/kemal/README.md
+++ crystal/kemal/README.md
@@ -1,9 +1,9 @@
 # Kemal
 
 - [x] Models linked to a PostgreSQL database through an ORM
-- [ ] CRUD actions to create a task, destroy a task, set a task as completed and get all existing tasks
+- [x] CRUD actions to create a task, destroy a task, set a task as completed and get all existing tasks
 - [ ] Authentication with encrypted password and Json Web Token for client side auth
 - [ ] Import and export of CSV files containing the tasks
 - [ ] Transactional tests with fixtures and 100% code coverage
-- [ ] Linting & Formating
-- [ ] CLI commands for database management
+- [x] Linting & Formating
+- [x] CLI commands for database management
--- crystal/kemal/src/app.cr
+++ crystal/kemal/src/app.cr
@@ -2,9 +2,56 @@ require "kemal"
 require "../config/config"
 
 Kemal.config.host_binding = "::1"
+add_context_storage_type(Task)
 
-get "/" do
+before_all do |env|
+  env.response.content_type = "application/json"
+end
+
+get "/tasks" do
   Task.all.to_json
+end
+
+post "/tasks" do |env|
+  task_params = env.params.json["task"].as Hash
+  title = task_params["title"].as_s?
+  task = Task.new({ title: title })
+
+  if task.save
+    env.response.status_code = 202
+    task.to_json
+  else
+    env.response.status_code = 422
+    { errors: task.errors.full_messages }.to_json
+  end
+end
+
+before_all "/tasks/:id" do |env|
+  if env.params.url["id"]?
+    id = env.params.url["id"]
+    task = Task.find(id)
+
+    if task.nil?
+      halt env, status_code: 422, response: ({ errors: ["Task must exist"] }.to_json)
+    end
+
+    env.set "task", task
+  end
+end
+
+delete "/tasks/:id" do |env|
+  task = env.get("task").as Task
+  task.destroy
+
+  env.response.status_code = 200
+end
+
+get "/tasks/:id/complete" do |env|
+  task = env.get("task").as Task
+  task.update(completed: true)
+
+  env.response.status_code = 200
+  task.to_json
 end
 
 Kemal.run
