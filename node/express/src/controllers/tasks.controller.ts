ff837bb8d08c83c5270a0e3371872e558629433f
Louis Ramos <louisramosdev@gmail.com>
2024-03-12 23:57:57 +0100

    express: refactor TasksController error handling

--- node/express/src/controllers/tasks.controller.ts
+++ node/express/src/controllers/tasks.controller.ts
@@ -15,11 +15,7 @@ export const create = async (req: Request, res: Response) => {
     const task = await prisma.task.create({ data })
     return res.status(201).json(task)
   } catch (error) {
-    if (error instanceof zod.ZodError) {
-      return res.status(422).json({ errors: error.issues.map(issue => issue.message) })
-    }
-
-    throw error
+    return _handleError(error, res)
   }
 }
 
@@ -28,7 +24,7 @@ export const destroy = async (req: Request, res: Response) => {
     await prisma.task.delete({ where: { id: +req.params.id } })
     return res.status(200).send()
   } catch (error) {
-    return res.status(422).send()
+    return _handleError(error, res)
   }
 }
 
@@ -37,10 +33,21 @@ export const complete = async (req: Request, res: Response) => {
     const task = await prisma.task.update({ data: { completed: true }, where: { id: +req.params.id } })
     return res.status(200).json(task)
   } catch (error) {
-    if (error instanceof zod.ZodError) {
-      return res.status(422).json({ errors: error.issues.map(issue => issue.message) })
-    }
-
-    throw error
+    return _handleError(error, res)
   }
+}
+
+const _handleError = (error: unknown, res: Response) => {
+  if (error instanceof zod.ZodError) {
+    return res.status(422).json({ errors: error.issues.map(issue => issue.message) })
+  }
+
+  if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    if (error.code === "P2025") {
+      // "An operation failed because it depends on one or more records that were required but not found."
+      return res.status(422).send()
+    }
+  }
+
+  throw error
 }
