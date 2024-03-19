d91b9ba7122c222c17f89c85a44cbb35c18053e6
Louis Ramos <louisramosdev@gmail.com>
2024-03-19 19:55:33 +0100

    sinatra: add csv seed

--- ruby/sinatra/db/seeds.rb
+++ ruby/sinatra/db/seeds.rb
@@ -1,1 +1,8 @@
-Task.create(title: "My task")
+require "csv"
+
+User.destroy_all
+
+user = User.create(email: "user@example.com", password: "password")
+CSV.read("#{__dir__}/static/tasks.csv", headers: true).each do |row|
+  Task.create!(title: row["title"], completed: row["completed"], user:)
+end
